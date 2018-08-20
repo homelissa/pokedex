c414b04e42d639ebf68eb2db67da2e9a78409a5c
Melissa Ho <homelissa93@gmail.com>
2018-08-20 14:50:29 -0700

    initial commit

---
+++ frontend/pokedex.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>This is Melissa's Pokedex</h1>, root)
+})
