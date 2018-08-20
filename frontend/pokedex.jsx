67cb416d79b437c1be9787db87ceb5137eaff5bc
Melissa Ho <homelissa93@gmail.com>
2018-08-20 15:28:13 -0700

    create actions, thunk, reducers, store, util

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  const store = configureStore()
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.getState = store.getState; window.dispatch = store.dispatch
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>This is Melissa's Pokedex</h1>, root)
+
 })
